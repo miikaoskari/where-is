a001ac4f2e8ec6b9344cb7d3cdbe82c7e83e9a3b
miikaoskari <miika.karjalainen13@gmail.com>
2025-04-28 23:14:30 +0300

    Handle textcolor also in themedtextinput

--- components/ThemedTextInput.tsx
+++ components/ThemedTextInput.tsx
@@ -4,18 +4,23 @@ import { TextInputProps, StyleSheet, TextInput } from "react-native";
 export type ThemedTextInputProps = TextInputProps & {
     lightColor?: string;
     darkColor?: string;
+    textLightColor?: string;
+    textDarkColor?: string;
     placeholderLightColor?: string;
     placeholderDarkColor?: string;
 };
 
 export function ThemedTextInput({
-    lightColor,
-    darkColor,
-    placeholderLightColor,
-    placeholderDarkColor,
+    lightColor = "#F2F2F2",
+    darkColor = "#2C2C2C",
+    textLightColor = "#000000",
+    textDarkColor = "#FFFFFF",
+    placeholderLightColor = "#888888",
+    placeholderDarkColor = "#CCCCCC",
     ...rest
 }: ThemedTextInputProps) {
     const color = useThemeColor({ light: lightColor, dark: darkColor }, 'text');
+    const textColor = useThemeColor({ light: textLightColor, dark: textDarkColor }, 'text');
     const placeholderColor = useThemeColor(
         { light: placeholderLightColor, dark: placeholderDarkColor },
         'text'
@@ -25,7 +30,7 @@ export function ThemedTextInput({
         <TextInput
             style={[
                 styles.input, 
-                { color, backgroundColor: color, borderColor: color },
+                { color: textColor, backgroundColor: color, borderColor: color },
             ]}
             placeholderTextColor={placeholderColor}
             {...rest}
