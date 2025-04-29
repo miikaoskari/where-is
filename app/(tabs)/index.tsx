75b3e474569c0435b58593493ebedfa21fcc3d14
miikaoskari <miika.karjalainen13@gmail.com>
2025-04-29 20:07:25 +0300

    Set ListItems view as index tab

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,74 +1,155 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import { BottomBarAwareView } from "@/components/BottomBarAwareView";
+import { ThemedText } from "@/components/ThemedText";
+import React, { useState, useEffect, useCallback } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  Alert,
+} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { ThemedTextInput } from "@/components/ThemedTextInput";
+import { FlashList } from "@shopify/flash-list";
+import { ThemedView } from "@/components/ThemedView";
+import { getAllItems } from "@/database/database";
+import { useFocusEffect, useRouter } from "expo-router";
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+interface Item {
+  id: number;
+  name: string;
+  description: string;
+  created_at: string;
+}
 
-export default function HomeScreen() {
+export default function ListItems() {
+  const [items, setItems] = useState<Item[]>([]);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [isRefreshing, setIsRefreshing] = useState(false);
+  const router = useRouter();
+
+  async function fetchItems() {
+    setIsRefreshing(true);
+    try {
+      const fetchedItems = await getAllItems();
+      setItems(fetchedItems as Item[]);
+    } catch (error) {
+      console.error("Error fetching items:", error);
+    } finally {
+      setIsRefreshing(false);
+    }
+  }
+
+  useFocusEffect(
+    useCallback(() => {
+      fetchItems();
+      return () => {};
+    }, [])
+  );
+
+  const handleItemPress = (item: Item) => {
+    router.push("/(modals)/EditItem");
+  };
+
+  const handleAddItem = () => {
+      router.push("/(modals)/AddNew");
+  };
+
+  const filteredItems = items.filter((item) =>
+    item.name.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12'
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+    <ThemedView lightColor="#FFFFFF" darkColor="#1C1C1E" style={{ flex: 1 }}>
+      <BottomBarAwareView style={{ flex: 1 }}>
+        <SafeAreaView style={styles.container}>
+          <View style={styles.topbarContainer}>
+            <ThemedText type="title" style={styles.title}>
+              Items
+            </ThemedText>
+
+            <TouchableOpacity
+              style={styles.addButton}
+              onPress={() => handleAddItem()}
+            >
+              <Text style={styles.addButtonText}>+</Text>
+            </TouchableOpacity>
+          </View>
+          <ThemedTextInput
+            placeholder="Item Name"
+            value={searchQuery}
+            onChangeText={setSearchQuery}
+          />
+
+          <FlashList
+            data={filteredItems}
+            onRefresh={fetchItems}
+            refreshing={isRefreshing}
+            renderItem={({ item, index }) => (
+              <React.Fragment>
+                <TouchableOpacity
+                  style={styles.itemContainer}
+                  onPress={() => handleItemPress(item)}
+                >
+                  <ThemedText type="default" style={styles.item}>
+                    {item.name}
+                  </ThemedText>
+                </TouchableOpacity>
+                {index < filteredItems.length - 1 && (
+                  <View style={styles.separator} />
+                )}
+              </React.Fragment>
+            )}
+            keyExtractor={(item) => item.id.toString()}
+            estimatedItemSize={50}
+          />
+        </SafeAreaView>
+      </BottomBarAwareView>
+    </ThemedView>
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
+  container: {
+    flex: 1,
+    padding: 12,
+    gap: 10,
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+  containerflex: {
+    flex: 1,
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+  card: {
+    backgroundColor: "#FFFFFF",
+    borderRadius: 12,
+    overflow: "hidden",
+  },
+  itemContainer: {
+    paddingVertical: 16,
+  },
+  item: {
+    fontSize: 17,
+    fontWeight: "500",
+  },
+  separator: {
+    height: 1,
+    backgroundColor: "#E0E0E0",
+  },
+  title: {
+    justifyContent: "flex-start",
+  },
+  addButton: {
+    borderRadius: 30,
+    justifyContent: "flex-end",
+  },
+  addButtonText: {
+    fontSize: 30,
+    color: "#007AFF",
+    fontWeight: "bold",
+  },
+  topbarContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    padding: 10,
   },
 });
