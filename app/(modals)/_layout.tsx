ffb6ca4fb379d377e1fa02bb037f3bd4de066abe
miikaoskari <miika.karjalainen13@gmail.com>
2025-05-01 22:14:43 +0300

    Fix navigation titles for modals

--- app/(modals)/_layout.tsx
+++ app/(modals)/_layout.tsx
@@ -1,21 +1,33 @@
 import React from 'react';
-import { Stack } from 'expo-router';
+import { Stack, useNavigation } from 'expo-router';
+import { Button } from 'react-native';
 
 export default function ModalLayout() {
+  const navigation = useNavigation();
+
   return (
     <Stack
       screenOptions={{
-        headerShown: false,
+        headerShown: true,
+        headerBackButtonMenuEnabled: true,
+        headerLeft: () => (
+          <Button
+            title="Back"
+            onPress={() => navigation.goBack()}
+          />
+        ),
       }}
     >
       <Stack.Screen
         name="EditItem"
         options={{
+          title: "Edit Item",
         }}
       />
       <Stack.Screen
         name="AddNew"
         options={{
+          title: "Add New Item",
         }}
       />
     </Stack>
